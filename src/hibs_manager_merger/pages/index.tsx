626e9bbd99f378ead7b947518549c8cd59fbf150
aagb1884 <a.a.g.blair@gmail.com>
2024-09-23 12:30:21 +0100

    css problems fixed, temp solution to restart game button not working

--- src/hibs_manager_merger/pages/index.tsx
+++ src/hibs_manager_merger/pages/index.tsx
@@ -3,8 +3,11 @@ import Score from "../components/score";
 import styles from "../styles/index.module.css";
 import '../styles/globals.css';
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function HibsMergePage() {
+  const [instructionsVisible, setInstructionsVisible] = useState(false)
+
   return (
     <div className={styles.twenty48}>
 
@@ -12,14 +15,27 @@ export default function HibsMergePage() {
         <h1 style={{color: 'var(--primary-text-color)'}}>Who's in the Hibs dugout?</h1>
         <Score />
       </div>
+      <div className="instructions-container">
+        <button className={styles.button} onClick={() => setInstructionsVisible(!instructionsVisible)}>How do I play this?</button>
+        {instructionsVisible && (
+          <div className="instructions">
+          <p>You have a 4 x 4 grid, starting with two tiles showing a picture of Pat Fenlon.</p>
+          <p>You can either use the arrow keys on the keyboard or flick your touchscreen in the direction you want all the tiles to move.</p>
+          <p>If two identical tiles collide, they will merge into a new tile showing the next Hibs manager.</p>
+          <p>The aim of the game is to reach Jack Ross.</p>
+          </div>
+        )}
+      </div>
+      <br />
       <main className="hibs-merge-main">
         <Board />
       </main>
      
       <footer className="hibs-merge-footer">
         
-        <div>Based on{" "} 
+        <div>All the actual clever bits of coding come from{" "} 
         <a
+            className="credit"
             href="https://mateuszsokola.github.io/2048-in-react/"
             target="_blank"
             rel="noopener"
@@ -27,6 +43,7 @@ export default function HibsMergePage() {
             this 2048 game
         </a> by{" "} 
         <a
+            className="credit"
             href="https://github.com/mateuszsokola/2048-in-react"
             target="_blank"
             rel="noopener"
